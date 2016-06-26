fa125092f49ff923887e67d26d332a0e611d4aa0
Brian <briancorrea@gmail.com>
2016-06-26 15:21:43 -0400

    LoggedIn Notification

---
+++ yard-games-ios/Config.swift
@@ -0,0 +1,20 @@
+//
+//  Config.swift
+//  yard-games-ios
+//
+//  Created by Brian Correa on 6/25/16.
+//  Copyright © 2016 Milkshake Tech. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseUrl = "http:localhost:3000"
+    
+    //Notification:
+    static let kUserLoggedInNotification = "UserLoggedInNotification"
+    
+    static let padding = 20
+    static let origin_y = 120
+}
