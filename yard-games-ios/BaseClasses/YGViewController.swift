4d73173ac842375e9aaf7e22f235f5ad45a85c41
Brian <briancorrea@gmail.com>
2016-06-26 15:31:00 -0400

    Adding LoggedIn Observer

--- yard-games-ios/BaseClasses/YGViewController.swift
+++ yard-games-ios/BaseClasses/YGViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class YGViewController: UIViewController {
+    
+    static var currentUser = YGProfile()
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self,
+                                       selector: #selector(YGViewController.userLoggedIn(_:)),
+                                       name: Constants.kUserLoggedInNotification,
+                                       object: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("CurrentUser: \(YGViewController.currentUser.id)")
+        
+    }
+    
+    func userLoggedIn(notification: NSNotification){
+        if let user = notification.userInfo!["user"] as? Dictionary<String, AnyObject>{
+            YGViewController.currentUser.populate(user)
+        }
+    }
+    
+    func postLoggedInNotification(currentUser: Dictionary<String, AnyObject>){
+        let notification = NSNotification(
+            name: Constants.kUserLoggedInNotification,
+            object: nil,
+            userInfo: ["user":currentUser]
+        )
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.postNotification(notification)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
